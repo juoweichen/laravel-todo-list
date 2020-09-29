7a7a5f4b90ddcc32d612ed36e787a6b1d4fa3cc3
pootitan <pootitan@gmail.com>
2020-09-29 08:29:13 +0800

    update RM and some layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 			@foreach($todos as $todo)
 			<div class="card mt-2">
 				<div class="card-body p-2 d-flex">
-					<a href='/todo/{{$todo->id}}' class="mb-0">{{$todo->todo}}</p>
+					<a href='/todo/{{$todo->id}}' class="ml-3 mt-2">{{$todo->todo}}</a>
 						<a href='/todo/{{$todo->id}}/edit' class='btn btn-dark ml-auto'>Edit</a>
 						<a href='/todo/{{$todo->id}}/delete' class='btn btn-danger'>Delete</a>
 						<!-- <form action="/todo/{{$todo->id}}" mothod='POST'>
